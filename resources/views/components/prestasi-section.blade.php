ff89a6a89c89825195eb1122699f12e89170ccc0
rafifArka <rafifdwiarka123@gmail.com>
2025-08-08 16:55:28 +0700

    add "lihat lebih banyak" button

--- resources/views/components/prestasi-section.blade.php
+++ resources/views/components/prestasi-section.blade.php
@@ -1,7 +1,7 @@
 <!-- nanti coba di map() kalo ada, blm coba  -->
-<div class=' min-h-full w-full flex flex-col justify-start pt-10 items-center gap-20 pb-30'>
+<div class=' min-h-full w-full flex flex-col justify-start pt-10 items-center gap-10 pb-30'>
     <h1 class='font-semibold text-5xl'>Prestasi </h1>
-    <div class='grid grid-cols-3 gap-4'>
+    <div class='grid grid-cols-3 gap-4 mt-10'>
         <x-prestasi-card/>
         <x-prestasi-card/>
         <x-prestasi-card/>
@@ -9,4 +9,8 @@
         <x-prestasi-card/>
         <x-prestasi-card/>
     </div>
+    <div class='hover:-translate-y-2 transition-all duration-300 hover:shadow-xl'>
+        <a href='/prestasi'class=' bg-green-50 px-4 py-2 text-lg rounded-full border-2 text-green-600 border-green-600 font-semibold'>Lihat Lebih Banyak</a>
+    </div>
+
 </div>
